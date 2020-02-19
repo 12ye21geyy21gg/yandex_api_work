b6bc4da87213b227958bdefb7644b0b026daf367
12ye21geyy21gg <12ye2112ye21@gmail.com>
2020-02-19 16:56:36 +0300

    4 zadacha

--- main.py
+++ main.py
@@ -3,12 +3,28 @@ import sys
 import pygame
 import requests
 
-response = None
 pygame.init()
+width = 600
+height = 450
 screen = pygame.display.set_mode((600, 450))
+render_colors = [pygame.Color('yellow'), pygame.Color('orange'),
+                 pygame.Color('red')]
+render_modes = ['map', 'sat', 'skl']
+r = 30
+render_mode = 0
+response = None
 running = True
 x, y, d = 37.0, 57.0, 0.2
 isUpdated = False
+
+
+def draw_misc():
+    pygame.draw.circle(screen, render_colors[render_mode % 3],
+                       (width - r, height - r), r, 0)
+    font = pygame.font.Font(None, 10)
+    text = font.render(render_modes[render_mode % 3], 1, pygame.Color('black'))
+    screen.blit(text, (width - r - 10, height - r - 10))
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,6 +43,9 @@ while running:
                 d *= 0.1
             elif event.key == pygame.K_PAGEDOWN:
                 d *= 10
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                render_mode += 1
     if not isUpdated:
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&spn={d},{d}&l=sat"
         response = requests.get(map_request)
@@ -44,6 +63,8 @@ while running:
 
         # Рисуем картинку, загружаемую из только что созданного файла.
         screen.blit(pygame.image.load(map_file), (0, 0))
+        draw_misc()
+
     pygame.display.flip()
 pygame.quit()
 # Удаляем за собой файл с изображением.
