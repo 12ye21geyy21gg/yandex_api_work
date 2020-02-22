a7e9b01304dcf6c4cbe4918b5bee4a71aa5b36af
huscker <12ye21geyy21gg@mail.ru>
2020-02-22 14:27:25 +0300

    убрал баги

--- settings.py
+++ settings.py
@@ -1,38 +1,60 @@
-import sys
+import sys, os
 from pyui import Ui_Form
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtCore import QTimer
 
 
 class cart(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        # self.pushButton.clicked.connect(self.search)
-        # self.pushButton_2.clicked.connect(self.rest)
-        # self.pushButton_3.clicked.connect(self.cheche)
-        self.isRest = False
-        self.postal = False
-        self.adress = False
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.search)
+        self.pushButton_2.clicked.connect(self.rest)
+        self.pushButton_3.clicked.connect(self.cheche)
+        self.pushButton_4.clicked.connect(self.move)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(300)
+
 
     def rest(self):
         self.isRest = True
+        with open('taxi1', mode='w', encoding='utf8') as f:
+            f.write('reset:lol')
+
+    def update(self):
+        if os.path.isfile('taxi2'):
+            with open('taxi2', mode='r', encoding='utf8') as f:
+                t = f.read()
+                if t != self.textEdit.toPlainText():
+                    self.textEdit.setText(t)
+            # os.remove('taxi2')
 
     def search(self):
         self.S_O = self.lineEdit.text()
-        self.textEdit.setText()#то что нужно ввест
-    # to pygame :
-    def cheche(self): #
+        with open('taxi1', mode='w', encoding='utf8') as f:
+            f.write('search:' + self.S_O)
+
+    def cheche(self):
+        s = ''
         if self.checkBox.isChecked():
-            self.postal = True
+            s = s + '1'
         else:
-            self.postal = False
+            s = s + '0'
         if self.checkBox_2.isChecked():
-            self.adress = True
+            s = s + '1'
         else:
-            self.adress = False
+            s = s + '0'
+        with open('taxi1', mode='w', encoding='utf8') as f:
+            f.write('check:' + s)
+
+    def move(self):
+        with open('taxi1', mode='w', encoding='utf8') as f:
+            f.write(f'move:{self.lineEdit_2.text()},{self.lineEdit_3.text()},{self.lineEdit_4.text()}')
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = cart()
-    ex.show()
-    sys.exit(app.exec_())
+app = QApplication([])
+
+main = cart()
+main.show()
+sys.exit(app.exec_())
